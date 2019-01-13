8a6a6b76e158d1cc565eda01c18f64933c153ec3
David Stewart <davidstewart@gmail.com>
2019-01-13 22:08:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const codes = [
 const input = document.body;
 // listens for any key press, calls the keyInt function with the key pressed... if UpArro is pressed the keyInt Function will be called. you then need to call the properties of key to to that we write key.key which is the actual key pressed so pressing the down arrow will equals "ArrowDown"
 
-input.addEventListener('keydown', check) 
+
 
 // golbal counter that the function keyInt interates up then resets to 0 upon failure of the correct key punches 
 
@@ -25,7 +25,7 @@ function check(key) {
     count ++
     console.log(count)
     if (count === codes.length) {
-      init()
+      alert("Congrats")
     }
     return
   } else {
@@ -35,6 +35,8 @@ function check(key) {
 }
 
 function init (key) {
+ input.addEventListener('keydown', check) 
+ 
  if (count === codes.length) {
   console.log("Congrats Log")
    alert("Congrats")
